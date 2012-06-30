694cfe3b2985ece5b85e4f744a7a7835eaf1b046
Andrew Shcheglov <godwindock@gmail.com>
2012-06-30 20:43:34 +0300

    Refactoring

--- lib/mongobzar/mapping/base_mapper.rb
+++ lib/mongobzar/mapping/base_mapper.rb
@@ -16,24 +16,8 @@ module Mongobzar
         mapping_strategy.build_dto(domain_object)
       end
 
-      def update_dto(dto, domain_object)
-        mapping_strategy.update_dto(dto, domain_object)
-      end
-
-      def build_dtos(domain_objects)
-        mapping_strategy.build_dtos(domain_objects)
-      end
-
       def build_domain_objects(dtos)
         mapping_strategy.build_domain_objects(dtos)
-      end
-
-      def update_dtos(dtos, domain_objects)
-        mapping_strategy.update_dtos(dtos, domain_objects)
-      end
-
-      def update_dto!(dto, domain_object)
-        mapping_strategy.update_dto!(dto, domain_object)
       end
     end
   end
