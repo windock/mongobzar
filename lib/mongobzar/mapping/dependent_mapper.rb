f5cee79d6570c9ec9cc29f2d2b6d4e59a5fb6850
Andrew Shcheglov <godwindock@gmail.com>
2012-07-01 16:55:38 +0300

    refactoring

--- lib/mongobzar/mapping/dependent_mapper.rb
+++ lib/mongobzar/mapping/dependent_mapper.rb
@@ -9,46 +9,16 @@ module Mongobzar
       attr_accessor :foreign_key
 
       def insert_dependent_collection(parent, domain_objects)
-        dict, dtos = mapping_strategy.build_dtos_collection(domain_objects)
-
-        dtos.each do |dto|
-          link_dto!(dto, parent)
-        end
-
-        dtos.each do |dto|
-          mongo_collection.insert(dto)
-        end
-        dict.each do |domain_object, dto|
-          update_domain_object_after_insert(domain_object, dto)
-        end
-      end
-
-      def update_domain_object_after_insert(domain_object, dto)
-        mapping_strategy.link_domain_object(domain_object, dto)
+        dtos = build_dtos_and_link(parent, domain_objects)
+        insert_dtos(dtos)
       end
 
       def update_dependent_collection(parent, domain_objects)
-        dtos = dependent_dtos_cursor(parent).to_a
-        dict = mapping_strategy.update_dtos_collection(dtos, domain_objects)
-
-        dtos = dict.values
-        dtos.each do |dto|
-          link_dto!(dto, parent)
-        end
-
         remove_dependent_dtos(parent)
-        dtos.each do |dto|
-          mongo_collection.insert(dto)
-        end
-        dict.each do |domain_object, dto|
-          update_domain_object_after_update(domain_object, dto)
-        end
-      end
 
-      def update_domain_object_after_update(domain_object, dto)
-        mapping_strategy.link_domain_object(domain_object, dto)
+        dtos = build_dtos_and_link(parent, domain_objects)
+        insert_dtos(dtos)
       end
-      private :update_domain_object_after_update
 
       def find_dependent_collection(parent)
         dtos = dependent_dtos_cursor(parent)
@@ -58,6 +28,7 @@ module Mongobzar
       def link_dto!(dto, owner_domain_object)
         dto[foreign_key] = owner_domain_object.id
       end
+      private :link_dto!
 
       def dependent_dtos_cursor(domain_object)
         mongo_collection.find(foreign_key => domain_object.id).to_a
@@ -66,6 +37,22 @@ module Mongobzar
 
       def remove_dependent_dtos(domain_object)
         mongo_collection.remove(foreign_key => domain_object.id)
+      end
+
+      def insert_dtos(dtos)
+        dtos.each do |dto|
+          mongo_collection.insert(dto)
+        end
+      end
+      private :insert_dtos
+
+      def build_dtos_and_link(parent, domain_objects)
+        domain_objects.map do |domain_object|
+          dto = mapping_strategy.build_dto(domain_object)
+          link_dto!(dto, parent)
+          mapping_strategy.link_domain_object(domain_object, dto)
+          dto
+        end
       end
 
     end
--- lib/mongobzar/mapping_strategy/entity_mapping_strategy.rb
+++ lib/mongobzar/mapping_strategy/entity_mapping_strategy.rb
@@ -75,24 +75,22 @@ module Mongobzar
         mapped_collection.domain_objects
       end
 
-      #TEST_ME
-      def build_dtos_collection(domain_objects)
-        mapped_collection = build_mapped_collection
-        mapped_collection.load_domain_objects(domain_objects)
-        dict = mapped_collection.dict
-        dtos = dict.values
-        [dict, dtos]
-      end
-
-      #TEST_ME
-      def update_dtos_collection(dtos, domain_objects)
-        mapped_collection = build_mapped_collection
-        mapped_collection.load_dtos(dtos)
-        mapped_collection.update(domain_objects)
-        mapped_collection.dict
-      end
-
       private
+        def build_dtos_collection(domain_objects)
+          mapped_collection = build_mapped_collection
+          mapped_collection.load_domain_objects(domain_objects)
+          dict = mapped_collection.dict
+          dtos = dict.values
+          [dict, dtos]
+        end
+
+        def update_dtos_collection(dtos, domain_objects)
+          mapped_collection = build_mapped_collection
+          mapped_collection.load_dtos(dtos)
+          mapped_collection.update(domain_objects)
+          mapped_collection.dict
+        end
+
         def build_mapped_collection
           Mapping::MappedCollection.new(self)
         end
