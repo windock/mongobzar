0b3f95a6a2ec789bc0e8fa7e752c8c5e2ba1393d
Andrew Shcheglov <godwindock@gmail.com>
2012-07-04 21:37:58 +0300

    Correct require of MappedCollection

--- lib/mongobzar.rb
+++ lib/mongobzar.rb
@@ -6,7 +6,6 @@ end
 
 require 'mongobzar/mapper/dependent_mapper'
 require 'mongobzar/mapper/document_not_found'
-require 'mongobzar/mapper/mapped_collection'
 require 'mongobzar/mapper/mapper'
 
 require 'mongobzar/mapping_strategy/entity_mapping_strategy'
@@ -14,4 +13,5 @@ require 'mongobzar/mapping_strategy/value_object_mapping_strategy'
 require 'mongobzar/mapping_strategy/simple_mapping_strategy'
 require 'mongobzar/mapping_strategy/polymorphic_mapping_strategy'
 
+require 'mongobzar/utility/mapped_collection'
 require 'mongobzar/utility/bson_id_generator'
