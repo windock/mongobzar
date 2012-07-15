f0e265a0ac2ce4605d2fa81d9bdfbd4638b9cf96
Andrew Shcheglov <godwindock@gmail.com>
2012-07-15 21:07:44 +0300

    Refactored MapperDecorator - pushed methods upward

--- lib/mongobzar/mapper/mapper.rb
+++ lib/mongobzar/mapper/mapper.rb
@@ -8,6 +8,20 @@ module Mongobzar
       def build_dto!(dto, domain_object)
       end
 
+      def build_domain_object(dto)
+        return nil unless dto
+        domain_object = build_new(dto)
+        build_domain_object!(domain_object, dto)
+        domain_object
+      end
+
+      def build_dto(domain_object)
+        return nil unless domain_object
+        dto = {}
+        build_dto!(dto, domain_object)
+        dto
+      end
+
       def update_dto(dto, domain_object)
         return nil unless domain_object
         update_dto!(dto, domain_object)
--- lib/mongobzar/mapper/mapper_decorator.rb
+++ lib/mongobzar/mapper/mapper_decorator.rb
@@ -11,20 +11,6 @@ module Mongobzar
         mapper.build_new(dto)
       end
 
-      def build_domain_object(dto)
-        return nil unless dto
-        domain_object = build_new(dto)
-        build_domain_object!(domain_object, dto)
-        domain_object
-      end
-
-      def build_dto(domain_object)
-        return nil unless domain_object
-        dto = {}
-        build_dto!(dto, domain_object)
-        dto
-      end
-
       def update_dto!(dto, domain_object)
         mapper.update_dto!(dto, domain_object)
       end
